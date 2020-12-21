69602c263e8cc26d08044016529379722b170f19
Vinay Sangwan <73075190+vinay4194@users.noreply.github.com>
2020-12-21 12:37:43 +0530

    Add files via upload

---
+++ get_data.php
@@ -0,0 +1,11 @@
+<?php
+
+   $con= mysqli_connect("localhost","root","","portfolio_form_data");
+
+   extract($_POST);
+   if(isset($_POST['send-msg'])){
+      $sql="INSERT INTO `form_data`( `name`, `email`, `subject`, `message`) VALUES ('$name','$email','$subject','$message')";
+      $query=mysqli_query($con,$sql);
+      header('location:index.php');
+   }
+?>
