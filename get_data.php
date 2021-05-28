c98862e5e476325ae20763e18474487d73ab758d
Vinay Sangwan <73075190+vinay4194@users.noreply.github.com>
2021-05-28 18:10:20 +0530

    Update get_data.php

--- get_data.php
+++ get_data.php
@@ -6,6 +6,6 @@
    if(isset($_POST['send-msg'])){
       $sql="INSERT INTO `form_data`( `name`, `email`, `subject`, `message`) VALUES ('$name','$email','$subject','$message')";
       $query=mysqli_query($con,$sql);
-      header('location:index.php');
+      header('location:index.html');
    }
 ?>
